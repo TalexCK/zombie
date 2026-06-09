38c1968f01f9717c4b675d71ca8ff237b16e7407
TalexCK <talexcks@gmail.com>
2026-06-09 15:36:50 +0800

    feat: add progress bar

--- include/pvz/GameWorld/GameWorld.hpp
+++ include/pvz/GameWorld/GameWorld.hpp
@@ -9,6 +9,8 @@
 #include "pvz/GameObject/GameObject.hpp"
 
 #include "pvz/Framework/TextBase.hpp"
+
+#include "pvz/Objects/ProgressBar.hpp"
 #include "pvz/utils.hpp"
 
 class GameWorld : public WorldBase
@@ -26,8 +28,48 @@ public:
 
   void CleanUp() override;
 
+  int getSunCount() const { return m_sunCount; }
+  void addSun(int count)
+  {
+    m_sunCount += count;
+  }
+  bool consumeSun(int count)
+  {
+    if (m_sunCount < count)
+      return false;
+    m_sunCount -= count;
+    return true;
+  }
+
+  bool decreaseBrains()
+  {
+    m_brains--;
+    if (m_brains <= 0)
+      return true;
+    return false;
+  }
+
+  bool updateStage()
+  {
+    m_stage++;
+    if (m_stage > 5)
+      return false;
+    return true;
+  }
+
+  void initBrains()
+  {
+    m_brains = 5;
+  }
+
 private:
   std::list<std::shared_ptr<GameObject>> m_objects;
+  int m_sunCount = 0;
+  int m_stage = 0;
+  int m_brains = 0;
+  std::shared_ptr<TextBase> m_sunText;
+  std::shared_ptr<TextBase> m_infoText;
+  std::shared_ptr<ProgressBar> m_progressBar;
 };
 
 #endif // !GAMEWORLD_HPP__
--- src/GameWorld/GameWorld.cpp
+++ src/GameWorld/GameWorld.cpp
@@ -1,10 +1,26 @@
 #include "pvz/GameWorld/GameWorld.hpp"
 #include "pvz/Objects/Background.hpp"
+#include "pvz/Objects/ProgressBar.hpp"
 
 void GameWorld::Init()
 {
   m_objects.clear();
   m_objects.push_back(std::make_shared<Background>());
+
+  updateStage();
+  addSun(150);
+  initBrains();
+  m_sunText = std::make_shared<TextBase>(SUN_COUNTER_X, SUN_COUNTER_Y);
+  m_sunText->SetText(std::to_string(getSunCount()));
+  m_sunText->SetColor(0, 0, 0);
+
+  m_infoText = std::make_shared<TextBase>(WINDOW_WIDTH - 100, WINDOW_HEIGHT - 40);
+  m_infoText->SetText("Stage 1/5  Brains: 5");
+  m_infoText->SetColor(255, 255, 255);
+
+  m_progressBar = std::make_shared<ProgressBar>();
+  m_objects.push_back(m_progressBar);
+  m_progressBar->setStage(m_stage);
 }
 
 LevelStatus GameWorld::Update()
